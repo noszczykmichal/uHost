e8ac90441f924dfa60a1d363e6a8a8623b4a494c
Michal <noszczykmichal@wp.pl>
2021-12-13 14:15:21 +0100

    added form validation on the start-hosting page

--- shared.js
+++ shared.js
@@ -1,6 +1,6 @@
 const backdrop=document.querySelector('.backdrop');
 const modal=document.querySelector('.modal');
-const selectPlanButtons=document.querySelectorAll('.button');
+const selectPlanButtons=document.querySelectorAll('.plan .button');
 const modalNoButton=document.querySelector('.modal__action--negative');
 const toggleButton=document.querySelector('.toggle-button');
 const mobileNav=document.querySelector('.mobile-nav');
@@ -34,4 +34,42 @@ toggleButton.addEventListener('click', function(){
     backdrop.classList.add('active');
 })
 
+// The start-hosting page -form validation
 
+const signupForm=document.querySelector('.signup-form');
+const selectSignupForm=document.querySelector('select');
+const inputsSignupForm=document.querySelectorAll('input');
+const checkboxSignupForm=document.querySelector('input[type="checkbox"]');
+
+signupForm.addEventListener('submit', function(event){
+    for(let input of inputsSignupForm){
+        //if any input is empty add class invalid
+        if(!input.value.trim()){
+            event.preventDefault();
+            input.value=""; //set incorrect value(whitespaces) to an empty string
+            input.classList.add('invalid');
+        }
+        //if e.g. on the second check they are filled in, remove class invalid 
+        if(input.value.trim()){
+            input.classList.remove('invalid');
+        }
+    }
+
+    // the same for select and checkbox
+    if(selectSignupForm.value==='null'){
+        event.preventDefault();
+        selectSignupForm.classList.add('invalid');
+    }
+    if(!checkboxSignupForm.checked){
+        event.preventDefault();
+        checkboxSignupForm.classList.add('invalid');
+    }
+
+    //removing class invalid for select and checkbox
+    if(selectSignupForm.value!=='null'){
+        selectSignupForm.classList.remove('invalid');
+    }
+    if(checkboxSignupForm.checked){
+        checkboxSignupForm.classList.add('invalid');
+    }
+})
